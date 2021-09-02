af36d116051f6be21603a82c2c005c7b0b0ea678
sonali-tubachi <51355690+sonali-tubachi@users.noreply.github.com>
2021-09-02 23:01:40 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,19 @@
+#Add implementation
+def add(x,y):
+    return x+y
+
+#Subtract implementation
+def subract(x,y):
+    return x-y
+
+#Multiply implementation
+def multiply(x,y):
+    return x*y
+
+#Divide implementation
+def divide(x,y):
+    if y==0:
+        return divide_by_0_error
+    else:
+        return x/y
+
